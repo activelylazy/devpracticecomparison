ac1fc3e85f1e573a91f2e8b8d7a756fe9b71a4fe
David <dave@activelylazy.co.uk>
2011-06-12 17:05:38 +0100

    Added basic client

--- client/src/main/java/uk/co/activelylazy/devpractice/client/Client.java
+++ client/src/main/java/uk/co/activelylazy/devpractice/client/Client.java
@@ -3,51 +3,61 @@ package uk.co.activelylazy.devpractice.client;
 import java.io.IOException;
 import java.net.URLEncoder;
 
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
-import org.eclipse.jetty.server.Request;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.AbstractHandler;
 
-public class Client {
+public class Client implements Servlet {
 
-	public Client() throws Exception {
-		Server server = new Server(9000);
-		server.setHandler(new AbstractHandler() {
-			
-			@Override
-			public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-				System.out.println("Got request for: "+target);
-			}
-		});
-		server.start();
-	}
+	private static final String CLIENT = "http://localhost:9000/";
+	private static final String SERVER = "http://localhost:8989/";
 	
-	public static void main(String[] args) {
+	private HttpClient client;
+	
+	public HttpClient getClient() { return client; }
+	public void setClient(HttpClient client) { this.client = client; }
+
+	@Override
+	public void destroy() {
+	}
+
+	@Override
+	public ServletConfig getServletConfig() {
+		return null;
+	}
+
+	@Override
+	public String getServletInfo() {
+		return null;
+	}
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
 		try {
-			Client client = new Client();
-			String response = makeRequest("http://localhost:8989/register?endpoint=" + URLEncoder.encode("http://localhost:9000/", "UTF-8"));
-			
-			
-		} catch (Throwable t) {
-			t.printStackTrace();
+			HttpGet get = new HttpGet(SERVER+"/register?endpoint="+URLEncoder.encode(CLIENT, "UTF-8"));
+			HttpResponse response = client.execute(get);
+			HttpEntity entity = response.getEntity();
+			String status = EntityUtils.toString(entity);
+			if ( ! status.trim().equals("OK")) {
+				throw new ServletException("Got response: "+status);
+			}
+		} catch (IOException e) {
+			throw new ServletException(e);
 		}
 	}
 
-	private static String makeRequest(String request) throws IOException, ClientProtocolException {
-		HttpClient client = new DefaultHttpClient();
-		HttpGet get = new HttpGet(request);
-		HttpResponse response = client.execute(get);
-		HttpEntity entity = response.getEntity();
-		return EntityUtils.toString(entity);
+	@Override
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		
 	}
+
 }
--- client/src/test/java/uk/co/activelylazy/devpractice/client/ClientTest.java
+++ client/src/test/java/uk/co/activelylazy/devpractice/client/ClientTest.java
@@ -1,8 +1,43 @@
 package uk.co.activelylazy.devpractice.client;
 
+import java.io.IOException;
+import java.io.StringBufferInputStream;
+
+import javax.servlet.ServletException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.lib.legacy.ClassImposteriser;
 import org.junit.Test;
 
 
 public class ClientTest {
 
+	private Mockery context = new Mockery() {{ setImposteriser(ClassImposteriser.INSTANCE); }};
+	
+	@Test public void
+	init_registers_client() throws ServletException, IOException {
+		final HttpClient httpClient = context.mock(HttpClient.class);
+		final HttpResponse response = context.mock(HttpResponse.class);
+		final HttpEntity entity = context.mock(HttpEntity.class);
+		final String OK = "OK";
+
+		context.checking(new Expectations() {{
+			oneOf(httpClient).execute(with(any(HttpGet.class))); will(returnValue(response));
+			oneOf(response).getEntity(); will(returnValue(entity));
+			oneOf(entity).getContent(); will(returnValue(new StringBufferInputStream(OK)));
+			allowing(entity).getContentLength(); will(returnValue(new Integer(OK.length()).longValue()));
+			allowing(entity).getContentType();
+		}});
+		
+		Client client = new Client();
+		client.setClient(httpClient);
+		client.init(null);
+		
+		context.assertIsSatisfied();
+	}
 }
